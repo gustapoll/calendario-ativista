1d78b0af47109883e670676fbebdaf04259d67eb
Gustavo Padilha Polleti <gustavo.polleti@nubank.com.br>
2021-10-22 00:05:48 -0300

    feat/create hashtags cloud function

---
+++ functions/constants.js
@@ -0,0 +1,22 @@
+const Constants = {
+    HASHTAG_LABELS: ['#coleraalegria', '#desenhospelademocracia', '#designativista', '#foragarimpoforacovid', '#projetemos', '#mariellepresente'],
+
+    TAGS: ['amazonia', 'aquecimento global', 'brumadinho', 'crime ambiental', 'desastre ambiental', 'fumaça', 'indígenas',
+        'meio ambiente', 'mineração', 'oleo ', 'petroleo', 'praia contaminada', 'queimadas', 'previdência',
+        'reforma trabalhista', 'arte', 'bolsonaro', 'brasil',
+        'brazilresists', 'caixa2dobolsonaro', 'cirogomes', 'cultura', 'democracia', 'dilma',
+        'direitoshumanos', 'ditaduranuncamais', 'diversidade', 'doria', 'educação', 'educaçãoinclusiva', 'eleicoes2018',
+        'eleições2018', 'elenunca', 'esquerda', 'facistas', 'feminismo', 'foratemer', 'golpista',
+        'grevegeral', 'haddad', 'história', 'homofobia', 'impeachmentbolsonaro', 'justiça', 'lgbt', 'lgbtq', 'liberdade',
+        'lulalivre', 'lula', 'machismo', 'mariellevive ', 'mexeucomumamexeucomtodas', 'moro', 'movimentonegro',
+        'mulherescontrabolsonaro', 'mulheresnapolitica', 'mulheresunidaspelademocracia', 'negra', 'ninguemsoltaamãodeninguem',
+        'quemmatoumarielle', 'racismo', 'redeglobo', 'resistencia',
+        'sociedade', 'tsunamidaeducação', 'vazajato', 'vidasnegrasimportam'
+    ].sort(),
+
+
+    //ID of the backup folder in Google Drive
+    FOLDER_ID: "1UlkoCZ1jjEdPI8CwC2dViBFI-QyUVaK7"
+}
+
+export default Constants
