106d26f37df5acf82556690f0f835b11040229e9
Gustavo Padilha Polleti <gustavo.polleti@nubank.com.br>
2021-11-06 15:28:39 -0300

    feat/add cors support

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 80
 
+var cors = require('cors')
+
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Calendario Ativista Server!')
